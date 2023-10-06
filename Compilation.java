9647875544c0ef9c0d202ab4ac2888aa11c86599
Nirmal Karthikeyan <nirmalakarthikeyan1503@gmail.com>
2023-10-06 16:39:49 +0100

    Added definition for Compilation

--- Compilation.java
+++ Compilation.java
@@ -1,3 +1,37 @@
-public class Compilation {
+import java.util.ArrayList;
 
+public class Compilation {
+    ArrayList<Album> albums;
+    ArrayList<Track> tracks;
+    ArrayList<String> artists;
+
+    public Compilation(ArrayList<Album> albums, ArrayList<Track> tracks, ArrayList<String> artists) {
+        this.albums = albums;
+        this.tracks = tracks;
+        this.artists = artists;
+    }
+
+    public ArrayList<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(ArrayList<Album> albums) {
+        this.albums = albums;
+    }
+
+    public ArrayList<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(ArrayList<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public ArrayList<String> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(ArrayList<String> artists) {
+        this.artists = artists;
+    }
 }
